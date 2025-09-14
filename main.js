57243aff64dbf4cb23bcf712a8e0ceaadf7361b4
Froggy1018 <jihe2023@gmail.com>
2025-09-14 02:35:11 +0000

    it runs now :D

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-audioCtx.resume();
-gainNode.gain.value = 0;
-
 const input = document.getElementById('input')
 
 //creates web audio api elements
@@ -10,7 +7,6 @@ const gainNode = audioCtx.createGain();
 
 //create Oscillator node
 const oscillator = audioCtx.createOscillator();
-
 //gainNode = controls volume of AudioContext
 //Oscillator will play frequencies based on the oscillating mathmatical graphs
 //(How computers can play pure tones)
@@ -18,19 +14,31 @@ oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
 
-gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
-oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-gainNode.gain.setValueAtTime(0, audioCtx.currentTime+1);
-
 oscillator.start();
 gainNode.gain.value = 0;
 
+notes = new Map();
+notes.set(C, 261.6);
+notes.set(D,293.7);
+notes.set(E,329.6);
+notes.set(F,349.2);
+notes.set(G,392.0);
+notes.set(A,440);
+notes.set(B,493.9);
+
 
 function frequency(pitch) {
-
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime+1);
 }
+
+audioCtx.resume();
+gainNode.gain.value = 0;
 
 function handle() {
 
     frequency(input.value);
+    var userInput = String(input.value);
+    frequency(notes.get(userInput));
 }
